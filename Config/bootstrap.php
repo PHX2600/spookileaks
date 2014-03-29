1fa7f433df5f4a8c9d760ead8ae127bdad4744ff
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-03-29 15:07:36 -0700

    Configuration changes and added Bootstrap and Fontawesome assets

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -96,3 +96,10 @@ CakeLog::config('error', array(
 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
 	'file' => 'error',
 ));
+
+// Load Composer autoload.
+require APP . '/Vendor/autoload.php';
+
+// Remove and re-prepend CakePHP's autoloader as Composer thinks it is the most important.
+spl_autoload_unregister(array('App', 'load'));
+spl_autoload_register(array('App', 'load'), true, true);
