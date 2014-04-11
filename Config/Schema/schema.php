f05d1518add429b3c2f7b9547a49fb6e212d15e4
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-04-11 12:18:44 -0700

    Updated schema.php to prevent duplicate usernames

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -26,14 +26,15 @@ class SpookiLeaksSchema extends CakeSchema {
 
 	public $users = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
-		'username' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'username' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'unique', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'password' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'role' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 64, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
-			'id_UNIQUE' => array('column' => 'id', 'unique' => 1)
+			'id_UNIQUE' => array('column' => 'id', 'unique' => 1),
+			'username_UNIQUE' => array('column' => 'username', 'unique' => 1)
 		),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
